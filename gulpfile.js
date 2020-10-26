f27ee5d08abac9923050d7d255ddc02020f61d07
高橋まり <62144776+mari-takahashi-github@users.noreply.github.com>
2020-10-26 20:59:46 +0900

    必要ファイルをアップロード

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+var webpackStream = require("webpack-stream");
+var webpack = require("webpack");
+var webpackConfig = require("./webpack.config");
+
+var AUTOPREFIXER_BROWSERS = [
+  'last 2 version',
+  'ie >= 9',
+  'iOS >= 7',
+  'Android >= 4.2'
+];
+
+var src = {
+  'scss': ['scss/**/*.scss', '!scss/**/_*.scss'],
+  'scssWatch': 'csssrc/**/*.scss',
+  'js': ['src/**/*.js']
+};
+
+var dest = {
+  'css': './'
+};
+
+//webpackによるバンドル
+gulp.task('bundle', function(){
+  return webpackStream(webpackConfig, webpack)
+  .pipe(gulp.dest('dist'));
+});
+
+//Sassのコンパイル
+gulp.task('sass', function(){
+  return gulp.src(src.scss)
+  .pipe(sass({outputStyle: 'compressed'})).on("error", sass.logError)
+  .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
+  .pipe(gulp.dest(dest.css));
+});
+
+//watchタスク
+gulp.task('watch', function() {
+  gulp.watch(src.scssWatch, gulp.task('sass'));
+  gulp.watch(src.js, gulp.task('bundle'));
+});
+
+//defaultタスク
+gulp.task('default',
+  gulp.series('bundle','sass','watch')
+);
